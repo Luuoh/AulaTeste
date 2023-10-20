a3b9a8b83fd0299218f86292fbf749d6938c7dee
Luiz Otavio <luizd.teo@outlook.com>
2023-10-20 20:39:20 -0300

    Criado página index e vendas

---
+++ PrimeiroProjeto08/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace PrimeiroProjeto08 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
